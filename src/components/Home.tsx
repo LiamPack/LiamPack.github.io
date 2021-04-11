28ef613dacbbacba05b8ef6a651465c336bbdea0
Liam Packer <liampacker@gmail.com>
2021-04-11 01:26:56 -0400

    home page update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -27,6 +27,15 @@ export const Home: React.VFC = () => (
       can be helpful or of use to someone. how that help or use is manifested, i
       have no clue, that's up to you probably.
     </P>
+    <P>
+      now, why am i making these goals clear at all? well first of all it seems
+      like a good thing to do, proper communication is important after all. but
+      more importantly to me, this is necessary to keep me honest. what i don't
+      want this place to be is somewhere i end up bragging about a lukewarm
+      understanding of topics out of my depth, that would be bad. what i do want
+      is a productive, home-grown corner of the internet that is an honest look
+      into how i approach and think about things.
+    </P>
     <h1>
       {' '}
       <i> Posts </i>{' '}
